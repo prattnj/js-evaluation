447ace0175a083d1db46088a8d1d620813bb9132
Noah Pratt <prattnj@gmail.com>
2024-02-15 11:44:26 -0700

    Debug

--- main.go
+++ main.go
@@ -7,18 +7,26 @@ import (
 )
 
 func main() {
+	devMode := false
+
 	var program Program
 
-	// For development
-	/*content, err := ioutil.ReadFile("test.json")
-	if err != nil {
-		return
-	}*/
-
-	err := json.Unmarshal([]byte(os.Args[1]), &program)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if devMode {
+		content, err := os.ReadFile("test8.json")
+		if err != nil {
+			return
+		}
+		err = json.Unmarshal(content, &program)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		err := json.Unmarshal([]byte(os.Args[1]), &program)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
 	// fmt.Println(program) // Parser
